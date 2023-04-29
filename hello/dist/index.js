72cacb7321f67b944cddcc559952bcaf3e13f60b
Jayakumar Jayaraman <jjayaraman@onvutech.com>
2023-04-29 10:07:13 +0100

    cache added

--- hello/dist/index.js
+++ hello/dist/index.js
@@ -14,10 +14,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getPrice = void 0;
 const axios_1 = __importDefault(require("axios"));
+const ioredis_1 = __importDefault(require("ioredis"));
+const redis = new ioredis_1.default();
 const getPrice = () => __awaiter(void 0, void 0, void 0, function* () {
     const response = yield axios_1.default.get('https://api.coindesk.com/v1/bpi/currentprice.json');
     console.log(response === null || response === void 0 ? void 0 : response.data);
+    const key = 'currency:cache';
+    yield redis.set(key, JSON.stringify(response.data));
+    const data = yield redis.get(key);
+    console.log('Data from Cache::: ', data);
+    return response === null || response === void 0 ? void 0 : response.data;
 });
 exports.getPrice = getPrice;
-(0, exports.getPrice)();
+void (0, exports.getPrice)();
 //# sourceMappingURL=index.js.map
--- hello/src/index.ts
+++ hello/src/index.ts
@@ -1,10 +1,18 @@
 import axios from 'axios'
+import Redis from 'ioredis'
+
+const redis = new Redis()
 
 export const getPrice = async (): Promise<any> => {
   const response = await axios.get(
     'https://api.coindesk.com/v1/bpi/currentprice.json'
   )
   console.log(response?.data)
+  const key = 'currency:cache'
+  await redis.set(key, JSON.stringify(response.data)) // cache the response
+  const data = await redis.get(key)
+  console.log('Data from Cache::: ', data)
+
   return response?.data
 }
 
